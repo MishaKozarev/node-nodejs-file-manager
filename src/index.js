664e94c6b7256a4657fd5326b9bd87b644d1522a
MishaKozarev <mishaaaaa1986@mail.ru>
2024-02-02 20:22:07 +0300

    feat: add interface and commands welcome and goodbye

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('File manager');
+import os from 'os';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function parsArguments() {
+  const argument = process.argv[2];
+  if (argument) {
+    return argument.replace('--username=', '');
+  }
+}
+const parsedArgument = parsArguments();
+
+const userName = parsedArgument? parsedArgument : "Unknown user";
+
+const pathToWorkingDirectory = os.homedir();
+
+rl.on('SIGINT', () => {
+  console.log(
+    `\n Thank you for using File Manager, ${userName}, goodbye! \n`,
+  );
+  rl.close();
+  process.exit();
+});
+
+console.log(`Welcome to the File Manager ${userName}!`);
+console.log(`You are currently in ${pathToWorkingDirectory}`);
